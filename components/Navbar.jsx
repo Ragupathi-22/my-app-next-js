c0be312ee45c2e05bea9065f0bab0264a2253324
Ragupathi-22 <eragupathi06@gmail.com>
2025-05-28 11:47:53 +0530

    pop5

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
 import ProfilePopover from "../components/ProfilePopover ";
+  const siteUrl = process.env.NEXT_PUBLIC_WC_SITE_URL;
 
 const Navbar = () => {
   const { router } = useAppContext();
@@ -13,7 +14,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700 relative">
-      <Link href="https://cwpteam.ntplstaging.com/Ragu/nextjs/rg/">
+      <Link href="{siteUrl}">
         <Image
           className="cursor-pointer w-8 md:w-15"
           src={assets.logo}
@@ -23,7 +24,7 @@ const Navbar = () => {
 
       {/* Desktop Menu */}
       <div className="hidden md:flex items-center gap-4 lg:gap-8">
-        <Link href="/" className="hover:text-gray-900 transition">Home</Link>
+        <Link href="{siteUrl}" className="hover:text-gray-900 transition">Home</Link>
         <Link href="/all-products" className="hover:text-gray-900 transition">Shop</Link>
         <Link href="/cart" className="hover:text-gray-900 transition">Cart</Link>
       </div>
@@ -40,7 +41,7 @@ const Navbar = () => {
       {menuOpen && (
         <div className="absolute top-16 left-0 w-full bg-white border-t border-gray-200 md:hidden z-10">
           <div className="flex flex-col items-start p-4 gap-3">
-            <Link href="/" className="hover:text-gray-900 transition">Home</Link>
+            <Link href="{siteUrl}" className="hover:text-gray-900 transition">Home</Link>
             <Link href="/all-products" className="hover:text-gray-900 transition">Shop</Link>
             <Link href="/cart" className="hover:text-gray-900 transition">Cart</Link>
           </div>
